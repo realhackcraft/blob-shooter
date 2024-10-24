7ab166e35ad6f69c65d665208b02bee2b822ac58
Hackcraft_ <hackcraft_@hotmail.com>
2024-10-24 19:25:27 -0400

    Fix packaging

--- webpack.build.config.js
+++ webpack.build.config.js
@@ -2,8 +2,9 @@ const path = require("path");
 
 module.exports = {
   entry: "./dist1/scripts/main.js",
+  mode: "production",
   output: {
-    filename: "bundle.js",
+    filename: "bundle.min.js",
     path: path.resolve(__dirname, "dist2"),
   },
 };
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -2,11 +2,12 @@ const path = require("path");
 
 module.exports = {
   entry: "./dist1/scripts/main.js",
+  mode: "development",
   optimization: {
     minimize: false,
   },
   output: {
-    filename: "bundle.js",
+    filename: "bundle.min.js",
     path: path.resolve(__dirname, "dist2"),
   },
 };
